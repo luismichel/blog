1311e8848548ff0e64519c35b10f8c64181ef10c
Luis Michel <luis.michel@binah.mx>
2011-06-09 18:10:35 -0500

    ejemplo de blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9b4aeed822ae2102755c876a63c32a48ff06ee5099b0edd63f9e56eefa9ffe1bd026c4c107f5698ecf898427c24558c5ff3be85fca4ff49be1556dd0f87f7ec2'
